a0cc571c5b6ff877c1480f6e98208fa8e0edb8be
VitorLimaRios <vitorlimarios999@gmail.com>
2021-09-09 20:24:47 -0300

    make it better

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const BodyParser = require('body-parser');
 const cors = require('cors');
+const BodyParser = require('body-parser');
 const { userRouter, favoriteRouter } = require('../routes');
 
 const app = express();
